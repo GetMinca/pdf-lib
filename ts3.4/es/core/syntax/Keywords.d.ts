e31174e7fc18e392b932579597087a075db94d09
Brett Nakashima <brettnak@gmail.com>
2024-05-24 14:44:10 -0700

    add all the distribution files

---
+++ ts3.4/es/core/syntax/Keywords.d.ts
@@ -0,0 +1,30 @@
+import CharCodes from "./CharCodes";
+export declare const Keywords: {
+    header: CharCodes[];
+    eof: CharCodes[];
+    obj: CharCodes[];
+    endobj: CharCodes[];
+    xref: CharCodes[];
+    trailer: CharCodes[];
+    startxref: CharCodes[];
+    true: CharCodes[];
+    false: CharCodes[];
+    null: CharCodes[];
+    stream: CharCodes[];
+    streamEOF1: CharCodes[];
+    streamEOF2: CharCodes[];
+    streamEOF3: CharCodes[];
+    streamEOF4: CharCodes[];
+    embeddedStream1: CharCodes[];
+    embeddedStream2: CharCodes[];
+    embeddedStream3: CharCodes[];
+    embeddedStream4: CharCodes[];
+    embeddedStream5: CharCodes[];
+    embeddedStream6: CharCodes[];
+    embeddedStream7: CharCodes[];
+    endstream: CharCodes[];
+    EOF1endstream: CharCodes[];
+    EOF2endstream: CharCodes[];
+    EOF3endstream: CharCodes[];
+};
+//# sourceMappingURL=Keywords.d.ts.map
